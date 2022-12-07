807290c5b92aace76404f25e641cb35893b90d8e
zhulibo <zhulibochn@gmail.com>
2022-12-07 12:35:47 +0800

    update gitignore

--- dist/assets/js-fragment.2d371925.js
+++
@@ -1,1 +0,0 @@
-function i(e,n=200,u=!1){let t=null,l=0;return function(){l++,u&&!t&&e.apply(this,arguments),t&&clearTimeout(t),t=setTimeout(()=>{(l>1||!u)&&e.apply(this,arguments),t=null},n)}}export{i as d};
--- dist/assets/setting.7a705450.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./pinia.cc224967.js";const o=e("setting",{state:()=>({fileHost:"http://files.jhdatatech.com/"}),getters:{getFileHost:t=>t.fileHost}});export{o as u};
