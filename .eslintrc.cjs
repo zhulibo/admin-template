29a76a110cd328b54d7cbbfb1831af84c06b19d4
zhulibo <zhulibochn@gmail.com>
2023-05-07 18:59:41 +0800

    update: vite@4.3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,28 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended"
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript'
   ],
-  "rules": {
-    "vue/multi-word-component-names": "off",
-    "@typescript-eslint/ban-ts-comment": "off" // 阻止使用@ts-ignore时的警告
+  'rules': {
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off' // 阻止使用@ts-ignore时的警告
   },
-  "overrides": [
+  overrides: [
     {
-      "files": [
-        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"
+      files: [
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
       ],
-      "extends": [
-        "plugin:cypress/recommended"
+      'extends': [
+        'plugin:cypress/recommended'
       ]
     }
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
+  parserOptions: {
+    ecmaVersion: 'latest'
   }
 }
